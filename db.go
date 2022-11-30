2f3692f36abd800ba3d04fddfee85c395edc25cd
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-11-30 20:42:41 +0900

    feat: db ping

--- db.go
+++ db.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+)
+
+var (
+	// ErrNoDBURI is returned when the DB_URI environment variable is not set.
+	ErrNoDBURI = errors.New("DB_URI not set")
+
+	// ErrLoadEnv is returned when the .env file cannot be loaded.
+	ErrLoadEnv = errors.New("error loading .env file")
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func (db *DB) getURI() (uri string) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(ErrLoadEnv)
+	}
+	uri = os.Getenv("DB_URI")
+	if uri == "" {
+		log.Fatal(ErrNoDBURI)
+	}
+	return uri
+}
+
+func (db *DB) Connect(ctx context.Context) (err error) {
+	opt := options.Client().ApplyURI(db.getURI())
+	if err = opt.Validate(); err != nil {
+		return err
+	}
+	db.client, err = mongo.Connect(ctx, opt)
+	return err
+}
+
+func (db *DB) Disconnect(ctx context.Context) error {
+	return db.client.Disconnect(ctx)
+}
+
+func (db *DB) Ping(ctx context.Context) (err error) {
+	if err = db.client.Ping(ctx, nil); err != nil {
+		return err
+	}
+	fmt.Println("Ping to MongoDB successful")
+	return nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"time"
+)
 
 func main() {
-	uri, err := loadURI()
-	if err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	db := &DB{}
+	if err := db.Connect(ctx); err != nil {
 		log.Fatal(err)
 	}
-	log.Println(uri)
+	defer func(db *DB, ctx context.Context) {
+		if err := db.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(db, ctx)
+
+	if err := db.Ping(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
