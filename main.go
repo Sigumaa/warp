3e3311a26bb7c65c2d7ca6e7a2a4f7d3bfa2a61b
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-12-01 10:33:05 +0900

    feat: add link

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"errors"
+	"fmt"
 	"github.com/Sigumaa/warp/db"
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
+
+var (
+	ErrArgs = errors.New("two arguments are required. first is the path, second is the URI")
+	ErrURI  = errors.New("please provide a valid URI")
+)
+
+func isURL(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func parseArgs() (db.Link, error) {
+	link := db.Link{}
+
+	if len(os.Args) < 3 {
+		return link, ErrArgs
+	}
+	if !isURL(os.Args[2]) {
+		return link, ErrURI
+	}
+
+	link.Before = os.Args[1]
+	link.After = os.Args[2]
+
+	return link, nil
+}
 
 func main() {
 	ctx := context.TODO()
@@ -23,6 +55,35 @@ func main() {
 
 	if err := myDB.Ping(ctx); err != nil {
 		log.Fatal(err)
+	}
+
+	// この辺めちゃくちゃ気持ち悪い、そもそも追加処理自体を別のツールにするべきかなぁ
+	fmt.Println("do you want to add a link? (y/n)")
+	var answer string
+	bufio.NewScanner(os.Stdin).Scan()
+	answer = bufio.NewScanner(os.Stdin).Text()
+	if strings.ToLower(answer) == "y" || strings.ToLower(answer) == "yes" {
+		for {
+			link, err := parseArgs()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if err := myDB.AddLink(ctx, link); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("link added")
+
+			fmt.Println("do you want to add another link? (y/n)")
+			bufio.NewScanner(os.Stdin).Scan()
+			answer = bufio.NewScanner(os.Stdin).Text()
+			if strings.ToLower(answer) == "y" || strings.ToLower(answer) == "yes" {
+				continue
+			} else {
+				break
+			}
+		}
 	}
 
 	e := echo.New()
